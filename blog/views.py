a16c876b06b8ee65925557232619cc157d7679d2
YashwanthK <yashwanthkothakota@gmail.com>
2024-08-05 09:26:10 +0530

    add forms and form validation

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,34 @@ from django.http import Http404
 
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
+
+
+def post_share(request, post_id):
+    #  Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id = post_id,
+        status = Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # send ...email
+    else:
+        form = EmailPostForm()
+    return render(
+            request,
+            'blog/post/share.html',
+            {
+                'post':post,
+                'form':form
+            }
+    )
+
 class PostListView(ListView):
     '''
         Alternate post list view
