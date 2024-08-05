21d50399658056a9482519e953b9face72fde99a
YashwanthK <yashwanthkothakota@gmail.com>
2024-08-05 13:59:50 +0530

    add comment system

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,34 @@ from django.http import Http404
 
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id = post_id,
+        status = Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post':post,
+            'form':form,
+            'comment':comment
+        }
+    )
+
 
 def post_share(request, post_id):
     #  Retrieve post by id
@@ -95,8 +120,16 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+    # List of active comments
+    comments = post.comments.filter(active=True)
+    # form for users to comment
+    form = CommentForm()
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+        }
     )
