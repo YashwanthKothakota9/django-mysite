453c3c4a3ac61e191c2b3b238758e42f4971a5e3
YashwanthK <yashwanthkothakota@gmail.com>
2024-08-12 16:55:19 +0530

    add tags view inside list posts and also a tag manager using django-taggit

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.conf import settings
 from django.urls import reverse
 
+from taggit.managers import TaggableManager
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return (
@@ -32,6 +34,7 @@ class Post(models.Model):
     )
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
     
     # This class defines metadata for model
     class Meta:
